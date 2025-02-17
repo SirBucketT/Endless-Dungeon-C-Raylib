21cb2a29c32660c9b0d39e317da5d54ff8563a1b
SirBucketT <tkpriva@icloud.com>
2025-02-17 11:16:42 +0100

    update

--- UI.c
+++ UI.c
@@ -1,17 +1,16 @@
-//
-// Created by theodor on 2025-02-17.
-//
 #include "header.h"
 #include "raylib.h"
 
-void ScreenRender() {
+void ScreenRender(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Endless Dungeon");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
+
         DrawText("Welcome to the Endless Dungeon!", 100, 100, 20, RED);
+
         EndDrawing();
     }
 
--- header.h
+++ header.h
@@ -1,9 +1,6 @@
-//
-// Created by theodor on 2025-02-17.
-//
-
 #ifndef HEADER_H
 #define HEADER_H
+
 #define SCREEN_WIDTH    1800
 #define SCREEN_HEIGHT   900
 
